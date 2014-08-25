bff63bd0d7250cf98dd55566adfaee9b2279218a
Amy Vaillancourt-Sals & Matthew Williams <student@epicodus.com>
2014-08-25 10:48:24 -0700

    add error & success message display after new list submit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,8 +1,13 @@
 class ListsController < ApplicationController
 
   def index
+    @list = List.new
     @lists = List.all
     render('lists/index.html.erb')
+  end
 
+  def saved
+    @list = List.create(:name => params[:name])
+    render('lists/index.html.erb')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Wikipages::Application.routes.draw do
   match('/', {:via => :get, :to => 'lists#index'})
+  match('/lists', {:via => :post, :to => 'lists#saved'})
 end
 
